ca184b338738235bdf4a22d85d8015ce067ae802
EwoutVermeulen <wdvermeulen@gmail.com>
2018-12-07 01:08:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# teller2
+# SSR-Leiden Teller server
+
+NodeJS websockets server om de communicatie van teller.ssr-leiden.nl te regelen. Het doel is de hoeveelheid leden in het pand bij te houden. 
+
+## Om te beginnen
+
+Met de volgende instructies kan je het project lokaal draaien. 
+
+### Vereisten
+
+* `npm` versie 3.10.10
+* `node` versie 6.11.3
+
+### Installeren
+
+Navigeer met de terminal naar de server folder. Voer het volgende commando uit om `node_modules` te ontvangen:
+
+```
+npm i
+```
+
+Om de server te activeren en te debuggen voer het volgende commando uit:
+
+```
+node --inspect server.js
+```
+
+Daarmee draait de server op `localhost` (127.0.0.1) op port 8001 en het inspecteerbare gedeelte draait op hetzelfde adres op port 9229
+
+## Deployment
+
+Moet nog gebeuren.
+
+Pas in de file `/client/js/index.js` de `serverUrl` aan naar de locatie waar de server draait en doe dit bij voorkeur over `wss`
+
+## To-do
+* Inloggen veiliger maken
+* Logging bijhouden in de server
+* Opsplitsen `/client/js/index.js` in meerdere bestanden
+
+## Schrijvers
+
+* **Ewout Vermeulen** - *Eerste versie* - [mail](wdvermeulen@gmail.com)
+
+## Erkenningen
+
+* Geïnspireerd op een andere teller gemaakt door Joost van Someren
---
+++ server.js
@@ -0,0 +1,87 @@
+'use strict';
+const WebSocketServer = require('ws').Server;
+const Crypto = require('crypto-js');
+
+const wss = new WebSocketServer({ port: 8081 });
+const maxPeople = 330;
+const minPeople = 0;
+let people = 0;
+
+let lastRecorded = 0;
+function parseInput(number) {
+    if (isNaN(number)) number = lastRecorded;
+    else lastRecorded = number;
+    return Math.min(Math.max(number, minPeople), maxPeople);
+}
+
+wss.broadcast = function broadcastMsg(msg) {
+    wss.clients.forEach(function each(client) {
+        client.send(msg.toString());
+    });
+};
+
+setInterval(function() {
+    wss.broadcast('ping');
+}, 5000);
+
+wss.on('connection', ((client) => {
+	let verifiedUser = false;
+	let total = 0;
+	client.on('message', (message) => {
+	  	const messageContent = message.split(',');
+	  	const command = messageContent[0];
+        console.log(`received: ${message}`);
+	  	switch(command) {
+		    case 'set':
+		    	if (verifiedUser) {
+                    people = parseInput(messageContent[1]);
+                    console.log(people);
+                    total = 0;
+                    wss.broadcast(['set', people]);
+                }
+                else {
+                    console.log("No verified user");
+                }
+                break;
+            case 'add':
+                if (verifiedUser) {
+                    people = parseInput(people + parseInt(messageContent[1]) - total);
+                    total = messageContent[1];
+                    console.log(people);
+                    wss.broadcast(['set', people]);
+                }
+                else {
+                    console.log("No verified user");
+                }
+                break;
+			case 'reqSet':
+                total = 0;
+                client.send(['set', people].toString());
+                break;
+			case 'verify':
+                let string = messageContent[1];
+                let clientTotal = messageContent[2];
+                if ('0c03df324cb0926092cfec9b94c1c2c0a50799e00a89187807f84cc09de418f1' === Crypto.SHA256(string).toString()) {
+                    verifiedUser = true;
+                    client.send("verified");
+                }
+                else {
+                    client.send('unverified');
+                }
+                break;
+            case 'ping':
+                break;
+		}
+	});
+
+	client.on('end', () => {
+		console.log('Connection ended...');
+	});
+
+	client.onerror = function (err) {
+	  console.log('err: ', err);
+	};
+
+	client.send(['set', people].toString());
+    console.log('Client succesfully connected');
+}));
